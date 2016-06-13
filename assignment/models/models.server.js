e73db6e2774384bafa4857df97f42f84f7fea5a5
isha <srivastava.i@husky.neu.edu>
2016-06-13 12:10:51 -0700

    retouch asnmt5

--- assignment/models/models.server.js
+++ assignment/models/models.server.js
@@ -9,8 +9,8 @@
 
 module.exports = function()
 {
-    var mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost/cs5610summer1');
+    // var mongoose = require('mongoose');
+    // mongoose.connect('mongodb://localhost/cs5610summer1');
     
     var models={
         userModel:require("./user/user.model.server.js")(),
--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var app = express();
 
+var connectionString = 'mongodb://127.0.0.1:27017/test';
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cs5610summer1');
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
