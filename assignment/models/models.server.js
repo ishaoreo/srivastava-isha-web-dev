cf4515deebc1b1374066c86698914292d1bfb6de
isha <srivastava.i@husky.neu.edu>
2016-06-28 18:40:23 -0700

    openshift
    oo

--- assignment/models/models.server.js
+++ assignment/models/models.server.js
@@ -11,7 +11,22 @@ module.exports = function()
 {
     var mongoose = require('mongoose');
     mongoose.connect('mongodb://localhost/cs5610summer1');
-    
+
+    // var connectionString = 'mongodb://127.0.0.1:27017/test';
+var connectionString = 'mongodb://localhost/cs5610summer1';
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+
+mongoose.connect(connectionString);
+
+
     var models={
         userModel:require("./user/user.model.server.js")(),
         websiteModel: require("./website/website.model.server")(),
